01243de511dcfff5a8c4b52a4e6c1b5a30c8f9a3
jmaff <jmaff2003@gmail.com>
2017-08-12 17:04:28 -0400

    RuleBot v1.0
    results are sent in an embedded message so it looks nicer/is easier to distinguish

--- bot.py
+++ bot.py
@@ -1,13 +1,49 @@
-import discord
-import config
-import rules as r
+import rules
 
+import discord
 from discord.ext import commands
-import random
+
 
 description = '''Random bot i made just now'''
 bot = commands.Bot(command_prefix='!', description=description)
 
+def find_rule(text):
+    return handle_comment(text, rules.rules, rules.awards, True)
+
+
+def find_award(text):
+    return handle_comment(text, rules.rules, rules.awards, False)
+
+
+def handle_comment(comment, rules_list, awards_list, rule):
+    words = comment.lower().split(" ")
+    if rule:
+        terms = get_rules_list(rules_list, words)
+    else:
+        terms = get_rules_list(awards_list, words)
+
+    reply_text = ""
+
+    for term in terms:
+        reply_text += term[0] + ":\n" + term[1] + "\n"
+
+    return reply_text
+
+
+def get_rules_list(rules_list, query_list):
+    good_rules = []
+    for rule in rules_list:
+        is_good = 0
+        is_bad = 0
+        for query in query_list:
+            if (query not in rule[1].lower()) and (query != rule[0].lower()):
+                is_bad += 1
+            else:
+                is_good += 1
+        percent = (is_good / (is_good + is_bad))
+        if percent >= .75:
+            good_rules.append(rule)
+    return good_rules
 
 
 @bot.event
@@ -18,48 +54,19 @@ async def on_ready():
     print('------')
 
 
-@bot.command()
-async def rule(*, search : str):
-	await bot.say(find_rule(search)
-		      
-@bot.command()
-async def award(*, search : str):
-	await bot.say(find_rule(search)
- 
-
-def find_rule(text):
-	return handle_comment(text, rules.rules, rules.awards, True)
-		      
-def find_award(text):
-	return handle_comment(text, rules.rules, rules.awards, False)
+@bot.command(pass_context=True)
+async def rule(ctx, *, search: str):
+    rule_em = discord.Embed(title="Here's what I found for '{}'...".format(search), description=find_rule(search), color=0xFFA500)
+    rule_em.set_footer(text="{}".format(ctx.message.server.name), icon_url=ctx.message.server.icon_url)
+    await bot.say(embed=rule_em)
 
 
-def handle_comment(comment, rules_list, awards_list, rule):
-	 words = comment.lower().split(" ")
-	 if rule:
-	 	terms = get_rules_list(rules_list, words)
-	 else:
-	 	terms = get_rules_list(awards_list, words)
-	  
-	 reply_text = ""
-	  
-	 for term in terms:
-	 	reply_text += term[0] + ":\n" + term[1] + "\n"
-		  
-	 return reply_text
-	
-def get_rules_list(rules_list, query_list):
-  good_rules = []
-  for rule in rules_list:
-  	is_good = 0
-  	is_bad = 0
-  	for query in query_list:
-   		if (query not in rule[1].lower()) and (query != rule[0].lower()):
-        		is_bad+=1
-      		else:
-        		is_good+=1
-    	percent = ( is_good / (is_good+is_bad) )
-    	if percent >= .75:
-        	good_rules.append(rule)
-  return good_rules
+@bot.command(pass_context=True)
+async def award(ctx, *, search : str):
+    award_em = discord.Embed(title="Here's what I found for '{}'...".format(search), description=find_award(search), color=0xFFA500)
+    award_em.set_footer(text="{}".format(ctx.message.server.name), icon_url=ctx.message.server.icon_url)
+    await bot.say(embed=award_em)
+
+
+bot.run('')
 
