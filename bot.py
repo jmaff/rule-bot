7fefa57b3c83c0228063f297f6594e2f7b70970c
jmaff <jmaff2003@gmail.com>
2017-08-11 21:07:19 -0400

    Update bot.py

--- bot.py
+++ bot.py
@@ -6,7 +6,7 @@ from discord.ext import commands
 import random
 
 description = '''Random bot i made just now'''
-bot = commands.Bot(command_prefix='/', description=description)
+bot = commands.Bot(command_prefix='!', description=description)
 
 
 
@@ -20,41 +20,43 @@ async def on_ready():
 
 @bot.command()
 async def rule(*, search : str):
+	await bot.say(find_rule(search)
+		      
+@bot.command()
+async def award(*, search : str):
+	await bot.say(find_rule(search)
  
 
-	def handle_comment(comment, rules_list, awards_list):
-	 	words = comment.body.lower().split(" ")
-	 	terms = []
-	 	if (words[0].lower() == "!award"):
-	  		terms = get_rules_list(awards_list, words[1:])
-		  
-	 	elif (words[0].lower() == "!rule"):
-	  		terms = get_rules_list(rules_list, words[1:])
-		  
-	 	else:
-	  		return False
-		  
-	  	reply_text = ""
-	  
-	  	for term in terms:
-		  	reply_text += term[0] + ":\n" + term[1] + "\n"
-		  
-	  	return reply_text
-	
-	def get_rules_list(rules_list, query_list):
-  	good_rules = []
-  	for rule in rules_list:
-    		is_good = 0
-    		is_bad = 0
-    		for query in query_list:
-			if (query not in rule[1].lower()) and (query != rule[0].lower()):
-        			is_bad+=1
-      			else:
-        			is_good+=1
-    		percent = ( is_good / (is_good+is_bad) )
-    		if percent >= .75:
-        		good_rules.append(rule)
-    	return good_rules
+def find_rule(text):
+	return handle_comment(text, rules.rules, rules.awards)
 
-await bot.say(handle_comment(search, rules.rules, rules.awards)
+
+def handle_comment(comment, rules_list, awards_list, rule):
+	 words = comment.lower().split(" ")
+	 if rule:
+	 	terms = get_rules_list(rules_list, words)
+	 else:
+	 	terms = get_rules_list(awards_list, words)
+	  
+	 reply_text = ""
+	  
+	 for term in terms:
+	 	reply_text += term[0] + ":\n" + term[1] + "\n"
+		  
+	 return reply_text
+	
+def get_rules_list(rules_list, query_list):
+  good_rules = []
+  for rule in rules_list:
+  	is_good = 0
+  	is_bad = 0
+  	for query in query_list:
+   		if (query not in rule[1].lower()) and (query != rule[0].lower()):
+        		is_bad+=1
+      		else:
+        		is_good+=1
+    	percent = ( is_good / (is_good+is_bad) )
+    	if percent >= .75:
+        	good_rules.append(rule)
+  return good_rules
 
